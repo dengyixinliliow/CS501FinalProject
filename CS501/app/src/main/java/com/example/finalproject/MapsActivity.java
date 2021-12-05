8065bf1a1e064fa62713bed4fc7d2e0bf7feb862
hanyjasmine <hjasmine@bu.edu>
2021-12-05 16:37:17 -0500

    deleted test comment

--- CS501/app/src/main/java/com/example/finalproject/MapsActivity.java
+++ CS501/app/src/main/java/com/example/finalproject/MapsActivity.java
@@ -171,7 +171,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
 
         }
-        String test = "null";
 
     }
 
